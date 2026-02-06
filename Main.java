2b55afc066c5f4de603269a0929e47557e799b55
Pierpaolo Rolli <pierolli1996@gmail.com>
2026-02-06 16:25:01 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package veicolo;
+
+public class Main {
+
+    public static void main(String[] args) {
+    
+    Veicolo a=new Auto("Subaru",200,3);
+    Veicolo b=new Moto("Vespa",110);
+  
+      a.descrizione();
+   
+      b.descrizione();
+    
+    }
+
+}
